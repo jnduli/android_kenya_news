1ea1cffc9aaf5d51e08c9fade34c0f7698fc51fd
John Nduli <yohanaizraeli@gmail.com>
2017-07-02 10:33:02 +0300

    Added maps to link up to various sites rss feeds and linking a menu to its appropriate map

--- app/src/main/java/com/somekenyan/kenyanews/data/RSSLinks.java
+++ app/src/main/java/com/somekenyan/kenyanews/data/RSSLinks.java
@@ -1,10 +1,18 @@
 package com.somekenyan.kenyanews.data;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by rookie on 7/2/17.
  */
 
 public class RSSLinks {
+    public static Map<String, Map> supported_Sites(){
+        Map<String, Map> supported_sites = new HashMap<String, Map>();
+        supported_sites.put("Nation Kenya", nation_createMap());
+        return supported_sites;
+    }
     // NATION.co.ke RSS Links
     //latest news
     public static String NATION_LATEST = "http://www.nation.co.ke/latestrss.rss";
@@ -16,4 +24,19 @@ public class RSSLinks {
     public static String NATION_COUNTIES = "http://www.nation.co.ke/counties/1107872-1107872-view-asFeed-12p0keu/index.xml";
 
     public static String [] NATION_MENU = {"Latest","Politics","Business","Counties","Lifestyle","Sport", "Blogs & Opinions"};
+    public static Map<String, String> nation_createMap(){
+        Map<String, String> nationMap = new HashMap<String, String>();
+        nationMap.put("Latest", NATION_LATEST);
+        nationMap.put("Politics", NATION_POLITICS);
+        nationMap.put("Business",NATION_BUSINESS);
+        nationMap.put("Counties", NATION_COUNTIES);
+        nationMap.put("Lifestyle", NATION_LIFESTYLE);
+        nationMap.put("Sport", NATION_SPORT);
+        nationMap.put("Blogs & Opinions", NATION_BLOG);
+        return nationMap;
+    }
+
+    //TODO Standard Media RSS Links
+
 }
+
