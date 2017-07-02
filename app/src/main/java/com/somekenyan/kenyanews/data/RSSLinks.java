caae33b7acc0f04315ee69787ee0848e1914a922
John Nduli <yohanaizraeli@gmail.com>
2017-07-02 10:10:26 +0300

    Added nation rss links to the project

---
+++ app/src/main/java/com/somekenyan/kenyanews/data/RSSLinks.java
@@ -0,0 +1,19 @@
+package com.somekenyan.kenyanews.data;
+
+/**
+ * Created by rookie on 7/2/17.
+ */
+
+public class RSSLinks {
+    // NATION.co.ke RSS Links
+    //latest news
+    public static String NATION_LATEST = "http://www.nation.co.ke/latestrss.rss";
+    public static String NATION_BUSINESS = "http://www.nation.co.ke/business/996-996-view-asFeed-35lsruz/index.xml";
+    public static String NATION_POLITICS= "http://www.nation.co.ke/news/politics/1064-1064-view-asFeed-gogm2d/index.xml";
+    public static String NATION_LIFESTYLE = "http://www.nation.co.ke/lifestyle/1190-1190-view-asFeed-c7vy58/index.xml";
+    public static String NATION_BLOG = "http://www.nation.co.ke/oped/1192-1192-view-asFeed-47hl0nz/index.xml";
+    public static String NATION_SPORT = "http://www.nation.co.ke/sports/1090-1090-view-asFeed-u6rm5f/index.xml";
+    public static String NATION_COUNTIES = "http://www.nation.co.ke/counties/1107872-1107872-view-asFeed-12p0keu/index.xml";
+
+    public static String [] NATION_MENU = {"Latest","Politics","Business","Counties","Lifestyle","Sport", "Blogs & Opinions"};
+}
